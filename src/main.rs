c10f0071acb0e8548ac48e05ad1a3c001ddd82df
lohxx <lohanna.dev@gmail.com>
2020-04-14 00:49:12 -0300

    algoritimo de ordenação

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,15 @@ use std::env;
 use std::io::Write;
 use std::ffi::OsStr;
 use std::str::FromStr;
+use std::cmp::Ordering;
 use std::path::{Path, PathBuf};
+use std::fs::Metadata;
 
 use structopt::StructOpt;
 
 use console::*;
 
+const MAX_COLUMN_SIZE: usize = 6;
 const PINK_OUTPUT_FILES: [&str; 3] = ["png", "jpg", "jpge"];
 const RED_OUTPUT_FILES: [&str; 4] = ["tar.gz", "zip", "xz", "deb"];
 
@@ -44,11 +47,12 @@ struct Rs{
 #[derive(Debug)]
 struct FileDs {
     name: String,
-    metadata: std::fs::Metadata,
-    formated_name: Option<console::StyledObject<String>>,
+    metadata: Metadata,
+    formated_name: Option<StyledObject<String>>,
 }
 
 impl FileDs {
+    /// Define the color output of the files
     fn set_color(ext: &str, filename: &String) -> StyledObject<String> {
         for file_type in PINK_OUTPUT_FILES.iter() {
             if ext == *file_type {
@@ -64,22 +68,22 @@ impl FileDs {
         style(filename.clone()).white()	
     }
 
+    /// Initialize the struct
     fn new(name: String, metadata: std::fs::Metadata) -> Self {
-        let mut formated_name = None;
         let extension = Path::new(&name)
             .extension()
             .and_then(OsStr::to_str);
 
-        if metadata.is_dir() {
-            formated_name = Some(style(name.clone()).blue().bold());
-        }
-
-        match extension {
-            Some(e) => {
-                formated_name = Some(Self::set_color(&extension.unwrap(), &name));	
-            },
-            _ => ()
-        }
+        let formated_name = match extension {
+            Some(e) => Some(Self::set_color(&extension.unwrap(), &name)),
+            _ => {
+                if metadata.is_dir() {
+                    Some(style(name.clone()).blue().bold())
+                } else {
+                    Some(style(name.clone()).white())
+                }
+            }
+        };
 
         Self {
             name,
@@ -101,6 +105,28 @@ impl FromStr for Colors {
     }
 }
 
+
+fn sort(vec: &Vec<FileDs>) -> Vec<FileDs> {
+    let mut sorted_vec = Vec::new();
+    let mut index_vec = 0;
+    let mut index_vec2 = 1;
+    loop {
+        if vec[index_vec].name.to_lowercase() > vec[index_vec2].name.to_lowercase() {
+            sorted_vec.push(vec[index_vec2]);
+        } else {
+            sorted_vec.push(vec[index_vec]);
+        }
+        
+        index_vec += 1;
+        index_vec2 += 1;
+
+        if index_vec >= vec.len() {
+            break;
+        }
+    }
+    
+    sorted_vec
+}
 
 impl Rs {
     fn manager(&mut self) {
@@ -124,16 +150,32 @@ impl Rs {
             if let Ok(entry) = entry {
                 let metadata = entry.metadata().unwrap();
                 let name = entry.file_name().into_string().unwrap();
-
+                if name.starts_with(".") && !self.all{
+                    continue
+                }
                 files.push(FileDs::new(name, metadata));
             }
         }
 
-        for entry in files {
-        	println!("{:?}", entry.formated_name);
-        }      
-    }
+        println!("{:?}", sort(&files));
 
+		//files.sort_by(|a, b| a.name.cmp(&b.name));
+
+    //     let terminal = Term::stdout();
+    //     let mut output = String::new();
+    //     let mut chunks = files.chunks(MAX_COLUMN_SIZE);
+
+    //     while let chunk = chunks.next().unwrap() {
+    //         for row in chunk {
+    //             output.push_str(&row.name);
+    //             output.push_str(" ");
+    //         }
+            
+    //         terminal.write_line(&output).unwrap();
+    //         output = String::new();
+    //     };
+
+    // }
 }
 
 
